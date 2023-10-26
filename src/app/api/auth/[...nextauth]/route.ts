9ab8436a520f7fe7676fff7c954ab367a560d294
oldirtybratton <calebabratton@gmail.com>
2023-10-25 20:53:42 -0600

    update to route auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -11,13 +11,25 @@ const handler = NextAuth({
           scope: "profile",
         },
       },
+      issuer: "https://www.linkedin.com",
+      jwks_endpoint: "https://www.linkedin.com/oauth/openid/jwks",
+      profile(profile, tokens) {
+        const defaultImage =
+          "https://cdn-icons-png.flaticon.com/512/174/174857.png";
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture ?? defaultImage,
+        };
+      },
     }),
   ],
-  callbacks: {
-    session({ session, token, user }) {
-      return session; // The return type will match the one returned in `useSession()`
-    },
-  },
+  // callbacks: {
+  //   session({ session, token, user }) {
+  //     return session; // The return type will match the one returned in `useSession()`
+  //   },
+  // },
 });
 
 export { handler as GET, handler as POST };
