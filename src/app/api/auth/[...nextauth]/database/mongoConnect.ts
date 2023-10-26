fc40a1591b56c0c1e0535226c38771a55fe0c362
Caleb Bratton <caleb.bratton@dish.com>
2023-10-26 14:49:13 -0600

    added limited user

--- src/app/api/auth/[...nextauth]/database/mongoConnect.ts
+++ src/app/api/auth/[...nextauth]/database/mongoConnect.ts
@@ -1,6 +1,6 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 // Replace the placeholder with your Atlas connection string
-const uri = `mongodb+srv://vercel-admin-user:${process.env.DATABASE_PW}@cluster0.9mqkfg2.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://visitor:${process.env.DATABASE_PW}@cluster0.9mqkfg2.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 
 const mongoConnect = async () => {
