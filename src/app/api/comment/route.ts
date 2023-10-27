b803a0302de8f2f08b25d34f980d24398c5c8e3e
Caleb Bratton <caleb.bratton@dish.com>
2023-10-27 11:14:55 -0600

    added twilio integration

--- src/app/api/comment/route.ts
+++ src/app/api/comment/route.ts
@@ -1,10 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 import mongoConnect from "../auth/[...nextauth]/database/mongoConnect";
+import Twilio from "twilio";
 
 export async function POST(req: NextRequest) {
-  const client = await mongoConnect();
+  const mongoClient = await mongoConnect();
   const body = await req.json();
-  const collection = client.db("resume").collection("comments");
+  const collection = mongoClient.db("resume").collection("comments");
+  const authToken = process.env.TWILIO_TOKEN;
+  const accountSid = process.env.TWILIO_SID;
+  const client = Twilio(accountSid, authToken);
+
+  client.messages
+    .create({
+      body: body,
+      from: "+18556913924",
+      to: "+19253008699",
+    })
+    .then((message) => console.log(message.sid));
 
   const result = await collection.insertOne({
     name: body.name,
