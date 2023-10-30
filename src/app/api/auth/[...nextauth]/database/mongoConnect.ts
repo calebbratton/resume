bd5abcb50b9b404d8faee4a4e9b6a5c4d1bb5990
Caleb Bratton <caleb.bratton@dish.com>
2023-10-30 13:42:33 -0600

    updated pool size

--- src/app/api/auth/[...nextauth]/database/mongoConnect.ts
+++ src/app/api/auth/[...nextauth]/database/mongoConnect.ts
@@ -2,6 +2,9 @@ import { MongoClient } from "mongodb";
 const uri = `mongodb+srv://visitor:${process.env.DATABASE_PW}@cluster0.9mqkfg2.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 
-const client = new MongoClient(uri);
+const client = new MongoClient(uri, {
+  minPoolSize: 1,
+  maxPoolSize: 10,
+});
 
 export default client;
