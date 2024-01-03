fae3173d8e2a44c9c3757936158428cf01bd9959
oldirtybratton <calebabratton@gmail.com>
2024-01-03 12:31:58 -0700

    changed javascript to typescript in friends. added links to docs

--- src/app/components/Explorer/Friends.tsx
+++ src/app/components/Explorer/Friends.tsx
@@ -8,43 +8,51 @@ const Friends = () => (
       <div className="grid grid-cols-4 grid-flow-row gap-x-2 gap-y-4">
         {[
           {
-            name: "Javascript",
-            src: "/javascript.png",
+            name: "Typescript",
+            src: "/typescript.png",
+            link: "https://www.typescriptlang.org/docs/",
           },
           {
             name: "Golang",
             src: "/golang.png",
+            link: "https://go.dev/doc/",
           },
           {
             name: "React",
             src: "/react.png",
+            link: "https://react.dev/reference/react",
           },
           {
             name: "Java",
             src: "/java.png",
+            link: "https://docs.oracle.com/javase/8/docs/api/",
           },
           {
             name: "Python",
             src: "/python.png",
+            link: "https://docs.python.org/3/",
           },
           {
             name: "AWS",
             src: "/aws.png",
+            link: "https://docs.aws.amazon.com/",
           },
         ].map((item) => {
           return (
-            <div
-              key={item.name}
-              className="flex flex-col justify-center items-center"
-            >
-              <p>{item.name}</p>
-              <Image
-                src={item.src || "/defaultProfile.jpg"}
-                alt={item.name}
-                height={80}
-                width={80}
-              />
-            </div>
+            <a href={item.link} target="_blank">
+              <div
+                key={item.name}
+                className="flex flex-col font-bold justify-center items-center"
+              >
+                <p>{item.name}</p>
+                <Image
+                  src={item.src || "/defaultProfile.jpg"}
+                  alt={item.name}
+                  height={80}
+                  width={80}
+                />
+              </div>
+            </a>
           );
         })}
       </div>
